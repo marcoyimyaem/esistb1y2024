58d6265263836653220a0901448a726369690567
marcoyimyaem <marcoyimyaem@gmail.com>
2024-08-08 11:55:34 +0000

    day 3 java basics

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
-
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+            Plant p1 = new Plant();
+            Plant p2 = new Plant();
+           
+        
+    }
+}
---
+++ Plant.java
@@ -0,0 +1,24 @@
+/**@marcoyimyaem
+ * a plant class
+ * that will emulate plant growth*/
+public class Plant {
+    int numberOfLeaves;
+    int numberOfFruit;
+    // a plant class 
+   
+}
+
+class Fruit{
+    boolean isSweet;
+    boolean isSour;
+    // a Fruit class that serves the plant object
+
+    /* a Fruit 
+    *class 
+    *that 
+    *serves 
+    *the 
+    *plant 
+    *object
+    */
+}
