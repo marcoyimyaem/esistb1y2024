841640e777ed37fe0c038f3cc51ac2676343248e
marcoyimyaem <marcoyimyaem@gmail.com>
2024-08-16 11:53:48 +0000

    day 10

--- chapter2/LoopsLesson.java
+++ chapter2/LoopsLesson.java
@@ -12,6 +12,25 @@ public class LoopsLesson {
         }while(i>0);
             
         for(int y = 1;y<=10;System.out.println("y = "+ y++)){}
-            
+        String[] colors = {"red","black","blue","pink","white","green","yellow"};
+        for(String color: colors)
+            System.out.println("this is color "+color);
+        
+ROW:        for(int x = 1;x<=10;x++){
+COL:            for(int y=1;y<x;y++){
+                    if(y==5)
+                        break COL;
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+ROW2:        for(int x = 1;x<=10;x++){
+COL2:           for(int y=1;y<=10;y++){
+                    if(y==7)
+                        continue COL2;
+                    System.out.print(y*x+"\t");
+                    }
+                System.out.println();
+                }
     }
 }
